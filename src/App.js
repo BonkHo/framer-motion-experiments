85667e83299a6bebbd61bae1745c8f0b771b4b3a
John <John@DESKTOP-SRO1BKN>
2021-09-14 21:15:53 -0700

    Modal complete with click and drag

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { motion, AnimatePresence } from "framer-motion";
+import React, { useState } from "react";
+
+import Modal from "./components/Modal";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const close = () => setModalOpen(false);
+    const open = () => setModalOpen(true);
+
+    return (
+        <div className="App">
+            <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className="save-button"
+                onClick={() => (modalOpen ? close() : open())}
+            >
+                Launch Modal
+            </motion.button>
+
+            <AnimatePresence
+                inital={false}
+                exitBeforeEnter={true}
+                onExitComplete={() => null}
+            >
+                {modalOpen && (
+                    <Modal modalOpen={modalOpen} handleClose={close}></Modal>
+                )}
+            </AnimatePresence>
+        </div>
+    );
 }
 
 export default App;
